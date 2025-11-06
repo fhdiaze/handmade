1ee3d8e33d04107c19c18480f77c771311fc8333
fredy.diaz <fredy.diaz@2degrees.nz>
2025-11-06 19:15:26 +1300

    feat: update main

---
+++ src/handmade.c
@@ -0,0 +1,10 @@
+#include <stdlib.h>
+#include <windows.h>
+
+int CALLBACK WinMain([[__maybe_unused__]] HINSTANCE hInstance,
+                     [[__maybe_unused__]] HINSTANCE hPrevInstance,
+                     [[__maybe_unused__]] LPSTR lpCmdLine,
+                     [[__maybe_unused__]] int nCmdShow)
+{
+        return EXIT_SUCCESS;
+}
