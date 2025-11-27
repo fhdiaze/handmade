ee158d3d6dcb31a98b41378a973347079c0ce99e
fredy.diaz <fredy.diaz@2degrees.nz>
2025-11-27 19:02:03 +1300

    feat: square wave sound

--- src/handmade.c
+++ src/handmade.c
@@ -1,9 +1,19 @@
 #include "log.h"
+#include <cstddef>
+#include <dsound.h>
 #include <libloaderapi.h>
 #include <limits.h>
+#include <minwindef.h>
+#include <mmeapi.h>
+#include <stddef.h>
 #include <stdint.h>
+#include <windef.h>
 #include <windows.h>
+#include <winerror.h>
+#include <winnt.h>
 #include <xinput.h>
+
+typedef int64_t bool64;
 
 struct Win_WindowDimensions {
 	long width;
@@ -20,45 +30,118 @@ struct Win_OffScreenBuffer {
 
 static boolean global_running = false;
 static struct Win_OffScreenBuffer global_back_buffer;
+static LPDIRECTSOUNDBUFFER secbuffer;
 
-// NOTE(fredy): XInputGetState
 #define X_INPUT_GET_STATE(name)                                   \
 	DWORD WINAPI name([[__maybe_unused__]] DWORD dwUserIndex, \
 	                  [[__maybe_unused__]] XINPUT_STATE *pState)
 typedef X_INPUT_GET_STATE(x_input_get_state);
 X_INPUT_GET_STATE(XInputGetStateStub)
 {
-	return 0;
+	return ERROR_DEVICE_NOT_CONNECTED;
 }
 static x_input_get_state *XInputGetState_ = XInputGetStateStub;
 #define XInputGetState XInputGetState_
 
-// NOTE(fredy): XInputSetState
 #define X_INPUT_SET_STATE(name)                                   \
 	DWORD WINAPI name([[__maybe_unused__]] DWORD dwUserIndex, \
 	                  [[__maybe_unused__]] XINPUT_VIBRATION *pVibration)
 typedef X_INPUT_SET_STATE(x_input_set_state);
 X_INPUT_SET_STATE(XInputSetStateStub)
 {
-	return 0;
+	return ERROR_DEVICE_NOT_CONNECTED;
 }
 static x_input_set_state *XInputSetState_ = XInputSetStateStub;
 #define XInputSetState XInputSetState_
 
+#define DSOUND_CREATE(name) \
+	HRESULT WINAPI name(LPCGUID pcGuidDevice, LPDIRECTSOUND *ppDS, LPUNKNOWN pUnkOuter)
+typedef DSOUND_CREATE(direct_sound_create);
+
 static void win_xinput_load(void)
 {
-	HMODULE xinput_lib = LoadLibraryA("xinput1_3.dll");
+	HMODULE xinput_lib = LoadLibraryA("xinput1_4.dll");
+	if (!xinput_lib) {
+		xinput_lib = LoadLibraryA("xinput1_3.dll");
+	}
+
 	if (xinput_lib) {
-		XInputGetState = (x_input_get_state *)GetProcAddress(
-			xinput_lib, "XInputGetState");
+		XInputGetState = (x_input_get_state *)GetProcAddress(xinput_lib, "XInputGetState");
 		if (!XInputGetState) {
 			XInputGetState = XInputGetStateStub;
 		}
-		XInputSetState = (x_input_set_state *)GetProcAddress(
-			xinput_lib, "XInputSetState");
+		XInputSetState = (x_input_set_state *)GetProcAddress(xinput_lib, "XInputSetState");
 		if (!XInputSetState) {
 			XInputSetState = XInputSetStateStub;
 		}
+	}
+}
+
+/*
+* TODO(fredy): it looks like direct sound is deprecated
+*/
+static void win_dsound_init(HWND winhandle, size_t samples_per_sec, size_t buffersize)
+{
+	HMODULE dsound_lib = LoadLibraryA("dsound.dll");
+	if (!dsound_lib) {
+		// TODO(fredy): diagnostic
+		OutputDebugStringA("Error loading dsound.dll");
+	}
+
+	direct_sound_create *dsound_create =
+		(direct_sound_create *)GetProcAddress(dsound_lib, "DirectSoundCreate");
+	if (!dsound_create) {
+		// TODO(fredy): diagnostic
+		OutputDebugStringA("Error getting DirectSoundCreate function");
+	}
+
+	LPDIRECTSOUND direct_sound;
+	if (FAILED(dsound_create(nullptr, &direct_sound, nullptr))) {
+		// TODO(fredy): diagnostic
+		OutputDebugStringA("Error creating the handler for direct sound");
+	}
+
+	if (FAILED(IDirectSound_SetCooperativeLevel(direct_sound, winhandle, DSSCL_PRIORITY))) {
+		// TODO(fredy): diagnostic
+		OutputDebugStringA("Error setting the cooperative level for direct sound");
+	}
+
+	// NOTE(fredy): create the buffers
+	WAVEFORMATEX waveformat = {
+		.wFormatTag = WAVE_FORMAT_PCM,
+		.nChannels = 2,
+		.nSamplesPerSec = samples_per_sec,
+		.wBitsPerSample = 16,
+		.cbSize = 0,
+	};
+	waveformat.nBlockAlign = waveformat.nChannels * waveformat.wBitsPerSample / CHAR_BIT;
+	waveformat.nAvgBytesPerSec = waveformat.nSamplesPerSec * waveformat.nBlockAlign;
+
+	// NOTE(fredy): create the primary buffer
+	DSBUFFERDESC primbufferdesc = {};
+	primbufferdesc.dwSize = sizeof(primbufferdesc);
+	primbufferdesc.dwFlags = DSBCAPS_PRIMARYBUFFER;
+	LPDIRECTSOUNDBUFFER primbuffer;
+	if (FAILED(IDirectSound_CreateSoundBuffer(direct_sound, &primbufferdesc, &primbuffer,
+	                                          nullptr))) {
+		// TODO(fredy): diagnostic
+		OutputDebugStringA("Error creating the primary buffer (handle for the sound card)");
+	}
+
+	if (FAILED(IDirectSoundBuffer_SetFormat(primbuffer, &waveformat))) {
+		// TODO(fredy): diagnostic
+		OutputDebugStringA("Error setting the format for the primary buffer");
+	}
+
+	// NOTE(fredy): create the secondary buffer
+	DSBUFFERDESC secbufferdesc = {};
+	primbufferdesc.dwSize = sizeof(secbufferdesc);
+	primbufferdesc.dwBufferBytes = buffersize;
+	primbufferdesc.lpwfxFormat = &waveformat;
+	if (FAILED(IDirectSound_CreateSoundBuffer(direct_sound, &secbufferdesc, &secbuffer,
+	                                          nullptr))) {
+		// TODO(fredy): diagnostic
+		OutputDebugStringA("Error creating the secondary buffer");
 	}
 }
 
@@ -73,8 +156,8 @@ static struct Win_WindowDimensions win_window_get_dimensions(HWND winhandle)
 	return result;
 }
 
-static void render_weird_gradient(struct Win_OffScreenBuffer *buffer,
-                                  long blue_offset, long green_offset)
+static void render_weird_gradient(struct Win_OffScreenBuffer *buffer, long blue_offset,
+                                  long green_offset)
 {
 	uint8_t *row = (uint8_t *)buffer->memory;
 	for (long y = 0; y < buffer->height; ++y) {
@@ -94,8 +177,8 @@ static void render_weird_gradient(struct Win_OffScreenBuffer *buffer,
 /*
  * dib: device independent bitmap
  */
-static void win_resize_dib_section(struct Win_OffScreenBuffer *buffer,
-                                   long win_width, long win_height)
+static void win_resize_dib_section(struct Win_OffScreenBuffer *buffer, long win_width,
+                                   long win_height)
 {
 	if (buffer->memory) {
 		VirtualFree(buffer->memory, 0, MEM_RELEASE);
@@ -106,8 +189,7 @@ static void win_resize_dib_section(struct Win_OffScreenBuffer *buffer,
 
 	long bytes_per_pixel = 4;
 
-	buffer->bitmap_info.bmiHeader.biSize =
-		sizeof(buffer->bitmap_info.bmiHeader);
+	buffer->bitmap_info.bmiHeader.biSize = sizeof(buffer->bitmap_info.bmiHeader);
 	buffer->bitmap_info.bmiHeader.biWidth = buffer->width;
 	// NOTE(fredy): top-down layout (opposite to bottom-up).
 	// The first three bytes on the bitmap are for the top-left pixel
@@ -116,26 +198,24 @@ static void win_resize_dib_section(struct Win_OffScreenBuffer *buffer,
 	buffer->bitmap_info.bmiHeader.biBitCount = 32;
 	buffer->bitmap_info.bmiHeader.biCompression = BI_RGB;
 
-	long bitmap_memory_size =
-		buffer->width * buffer->height * bytes_per_pixel;
+	long bitmap_memory_size = buffer->width * buffer->height * bytes_per_pixel;
 
-	buffer->memory = VirtualAlloc(nullptr, (size_t)bitmap_memory_size,
-	                              MEM_COMMIT, PAGE_READWRITE);
+	buffer->memory = VirtualAlloc(nullptr, (size_t)bitmap_memory_size, MEM_RESERVE | MEM_COMMIT,
+	                              PAGE_READWRITE);
 	buffer->pitch = buffer->width * bytes_per_pixel;
 }
 
-static void win_buffer_display_in_window(struct Win_OffScreenBuffer *buffer,
-                                         HDC dchandle, long win_width,
-                                         long win_height)
+static void win_buffer_display_in_window(struct Win_OffScreenBuffer *buffer, HDC dchandle,
+                                         long win_width, long win_height)
 {
-	StretchDIBits(dchandle, 0, 0, win_width, win_height, 0, 0,
-	              buffer->width, buffer->height, buffer->memory,
-	              &buffer->bitmap_info, DIB_RGB_COLORS, SRCCOPY);
+	StretchDIBits(dchandle, 0, 0, win_width, win_height, 0, 0, buffer->width, buffer->height,
+	              buffer->memory, &buffer->bitmap_info, DIB_RGB_COLORS, SRCCOPY);
 }
 
-static LRESULT CALLBACK win_main_window_proc(
-	[[__maybe_unused__]] HWND winhandle, [[__maybe_unused__]] UINT msg,
-	[[__maybe_unused__]] WPARAM wparam, [[__maybe_unused__]] LPARAM lparam)
+static LRESULT CALLBACK win_main_window_proc([[__maybe_unused__]] HWND winhandle,
+                                             [[__maybe_unused__]] UINT msg,
+                                             [[__maybe_unused__]] WPARAM wparam,
+                                             [[__maybe_unused__]] LPARAM lparam)
 {
 	LRESULT result = 0;
 
@@ -177,14 +257,17 @@ static LRESULT CALLBACK win_main_window_proc(
 			} else if (vk_code == VK_SPACE) {
 			}
 		}
+		bool64 alt_key_was_down = (lparam & (1 << 29));
+		if ((vk_code == VK_F4) && alt_key_was_down) {
+			global_running = false;
+		}
 	} break;
 	case WM_PAINT: {
 		PAINTSTRUCT paint;
 		HDC dchandle = BeginPaint(winhandle, &paint);
-		struct Win_WindowDimensions windim =
-			win_window_get_dimensions(winhandle);
-		win_buffer_display_in_window(&global_back_buffer, dchandle,
-		                             windim.width, windim.height);
+		struct Win_WindowDimensions windim = win_window_get_dimensions(winhandle);
+		win_buffer_display_in_window(&global_back_buffer, dchandle, windim.width,
+		                             windim.height);
 		EndPaint(winhandle, &paint);
 	} break;
 	default: {
@@ -198,9 +281,10 @@ static LRESULT CALLBACK win_main_window_proc(
 
 int CALLBACK WinMain([[__maybe_unused__]] HINSTANCE hinstance,
                      [[__maybe_unused__]] HINSTANCE hprevinstance,
-                     [[__maybe_unused__]] LPSTR lpCmdLine,
-                     [[__maybe_unused__]] int nCmdShow)
+                     [[__maybe_unused__]] LPSTR lpCmdLine, [[__maybe_unused__]] int nCmdShow)
 {
+	win_xinput_load();
+
 	WNDCLASSA window_class = {
 		.style = CS_HREDRAW | CS_VREDRAW | CS_OWNDC,
 		.lpfnWndProc = win_main_window_proc,
@@ -217,11 +301,9 @@ int CALLBACK WinMain([[__maybe_unused__]] HINSTANCE hinstance,
 		return EXIT_FAILURE;
 	}
 
-	HWND winhandle = CreateWindowExA(0, MAKEINTATOM(main_window_atom),
-	                                 "Handmade Hero",
-	                                 WS_OVERLAPPEDWINDOW | WS_VISIBLE,
-	                                 CW_USEDEFAULT, CW_USEDEFAULT,
-	                                 CW_USEDEFAULT, CW_USEDEFAULT, nullptr,
+	HWND winhandle = CreateWindowExA(0, MAKEINTATOM(main_window_atom), "Handmade Hero",
+	                                 WS_OVERLAPPEDWINDOW | WS_VISIBLE, CW_USEDEFAULT,
+	                                 CW_USEDEFAULT, CW_USEDEFAULT, CW_USEDEFAULT, nullptr,
 	                                 nullptr, hinstance, nullptr);
 	if (!winhandle) {
 		// TODO(fredy): Logging
@@ -229,12 +311,26 @@ int CALLBACK WinMain([[__maybe_unused__]] HINSTANCE hinstance,
 		return EXIT_FAILURE;
 	}
 
-	MSG msg;
-	global_running = true;
+	HDC dchandle = GetDC(winhandle);
+
+	// NOTE(fredy): render test
 	int x_offset = 0;
 	int y_offset = 0;
-	HDC dchandle = GetDC(winhandle);
+
+	// NOTE(fredy): sound test
+	size_t samples_per_sec = 48000;
+	size_t tone_hz = 256;
+	size_t square_wave_period = samples_per_sec / tone_hz;
+	size_t half_square_wave_period = square_wave_period / 2;
+	size_t bytes_per_sample = sizeof(uint16_t) * 2;
+	size_t running_sample_index = 0;
+	size_t buffer_size = samples_per_sec * bytes_per_sample;
+
+	win_dsound_init(winhandle, samples_per_sec, buffer_size);
+
+	global_running = true;
 	while (global_running) {
+		MSG msg;
 		while (PeekMessageA(&msg, nullptr, 0, 0, PM_REMOVE)) {
 			if (msg.message == WM_QUIT) {
 				global_running = false;
@@ -245,49 +341,91 @@ int CALLBACK WinMain([[__maybe_unused__]] HINSTANCE hinstance,
 
 		for (WORD i = 0; i < XUSER_MAX_COUNT; ++i) {
 			XINPUT_STATE state;
-			if (XInputGetState(i, &state) == ERROR_SUCCESS) {
-				// Plugged in
-				XINPUT_GAMEPAD *pad = &state.Gamepad;
-				WORD up = (pad->wButtons &
-				           XINPUT_GAMEPAD_DPAD_UP);
-				WORD down = (pad->wButtons &
-				             XINPUT_GAMEPAD_DPAD_DOWN);
-				WORD left = (pad->wButtons &
-				             XINPUT_GAMEPAD_DPAD_LEFT);
-				WORD right = (pad->wButtons &
-				              XINPUT_GAMEPAD_DPAD_RIGHT);
-				WORD start =
-					(pad->wButtons & XINPUT_GAMEPAD_START);
-				WORD back =
-					(pad->wButtons & XINPUT_GAMEPAD_BACK);
-				WORD left_shoulder =
-					(pad->wButtons &
-				         XINPUT_GAMEPAD_LEFT_SHOULDER);
-				WORD right_shoulder =
-					(pad->wButtons &
-				         XINPUT_GAMEPAD_RIGHT_SHOULDER);
-				WORD a_button =
-					(pad->wButtons & XINPUT_GAMEPAD_A);
-				WORD b_button =
-					(pad->wButtons & XINPUT_GAMEPAD_B);
-				WORD x_button =
-					(pad->wButtons & XINPUT_GAMEPAD_X);
-				WORD y_button =
-					(pad->wButtons & XINPUT_GAMEPAD_Y);
-
-				SHORT stick_x = pad->sThumbLX;
-				SHORT stick_y = pad->sThumbLY;
-
-				x_offset = stick_x >> 12;
-				y_offset = stick_y >> 12;
+			if (XInputGetState(i, &state) != ERROR_SUCCESS) {
+				continue;
 			}
+
+			// Plugged in
+			XINPUT_GAMEPAD *pad = &state.Gamepad;
+			[[__maybe_unused__]] WORD up = (pad->wButtons & XINPUT_GAMEPAD_DPAD_UP);
+			[[__maybe_unused__]] WORD down = (pad->wButtons & XINPUT_GAMEPAD_DPAD_DOWN);
+			[[__maybe_unused__]] WORD left = (pad->wButtons & XINPUT_GAMEPAD_DPAD_LEFT);
+			[[__maybe_unused__]] WORD right =
+				(pad->wButtons & XINPUT_GAMEPAD_DPAD_RIGHT);
+			[[__maybe_unused__]] WORD start = (pad->wButtons & XINPUT_GAMEPAD_START);
+			[[__maybe_unused__]] WORD back = (pad->wButtons & XINPUT_GAMEPAD_BACK);
+			[[__maybe_unused__]] WORD left_shoulder =
+				(pad->wButtons & XINPUT_GAMEPAD_LEFT_SHOULDER);
+			[[__maybe_unused__]] WORD right_shoulder =
+				(pad->wButtons & XINPUT_GAMEPAD_RIGHT_SHOULDER);
+			[[__maybe_unused__]] WORD a_button = (pad->wButtons & XINPUT_GAMEPAD_A);
+			[[__maybe_unused__]] WORD b_button = (pad->wButtons & XINPUT_GAMEPAD_B);
+			[[__maybe_unused__]] WORD x_button = (pad->wButtons & XINPUT_GAMEPAD_X);
+			[[__maybe_unused__]] WORD y_button = (pad->wButtons & XINPUT_GAMEPAD_Y);
+
+			SHORT stick_x = pad->sThumbLX;
+			SHORT stick_y = pad->sThumbLY;
+
+			x_offset = stick_x >> 12;
+			y_offset = stick_y >> 12;
 		}
 
 		render_weird_gradient(&global_back_buffer, x_offset, y_offset);
-		struct Win_WindowDimensions windim =
-			win_window_get_dimensions(winhandle);
-		win_buffer_display_in_window(&global_back_buffer, dchandle,
-		                             windim.width, windim.height);
+
+		DWORD play_cursor;
+		DWORD write_cursor;
+		if (FAILED(IDirectSoundBuffer_GetCurrentPosition(secbuffer, &play_cursor,
+		                                                 &write_cursor))) {
+			// TODO(fredy): diagnostic
+			OutputDebugStringA(
+				"Error getting current position of dsound secondary buffer");
+			continue;
+		}
+
+		size_t byte_to_lock = running_sample_index * bytes_per_sample % buffer_size;
+		size_t bytes_to_write;
+		if (byte_to_lock > play_cursor) {
+			bytes_to_write = buffer_size - byte_to_lock;
+			bytes_to_write += play_cursor;
+		} else {
+			bytes_to_write = play_cursor - byte_to_lock;
+		}
+
+		void *region_one;
+		DWORD region_one_size;
+		void *region_two;
+		DWORD region_two_size;
+
+		if (FAILED(IDirectSoundBuffer_Lock(secbuffer, byte_to_lock, bytes_to_write,
+		                                   &region_one, &region_one_size, &region_two,
+		                                   &region_two_size, 0))) {
+			// TODO(fredy): diagnostic
+			OutputDebugStringA("Error locking dsound secondary buffer");
+			continue;
+		}
+
+		int16_t *sample_out = (int16_t *)region_one;
+		int16_t sample_value = 0;
+		DWORD region_sample_count = region_one_size / bytes_per_sample;
+		for (DWORD i = 0; i < region_sample_count; ++i) {
+			sample_value = (running_sample_index / half_square_wave_period) % 2 ?
+			                       16000 :
+			                       -16000;
+			*sample_out = sample_value; // channel one
+			++sample_out;
+			*sample_out = sample_value; // channel two
+			++sample_out;
+
+			++running_sample_index;
+		}
+
+		region_sample_count = region_two_size / bytes_per_sample;
+		for (DWORD i = 0; i < region_sample_count; ++i) {
+		}
+
+		struct Win_WindowDimensions windim = win_window_get_dimensions(winhandle);
+		win_buffer_display_in_window(&global_back_buffer, dchandle, windim.width,
+		                             windim.height);
 	}
 
 	return EXIT_SUCCESS;
